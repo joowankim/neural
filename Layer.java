e6299d0ec80ba93a138a3a4c4b9e9d951fde0cf7
kjw217 <32446834+kjw217@users.noreply.github.com>
2017-11-13 00:47:40 +0900

    Update Layer.java

--- Layer.java
+++ Layer.java
@@ -2,13 +2,29 @@ package neural_network;
 
 public class Layer {
 	Perceptron[] ptrons;
+	int ptrons_num;
 	double[] outputs;
+	double learningRate = 0.01;
 	
-	Layer(double[] input, int desired, int ptron_num) {
-		for(int i=0; i<ptron_num; i++) {
-			ptrons[i] = new Perceptron(input, desired);
+	Layer(double[] input, int ptron_num) {
+		ptrons_num	= ptron_num;
+		ptrons = new Perceptron[ptrons_num];
+		outputs = new double[ptrons_num];
+		
+		for(int i=0; i<ptrons_num; i++) {
+			ptrons[i] = new Perceptron(input);
 			outputs[i] = ptrons[i].feedforward();
 		}
 	}
 
+	void adjustWeight(double desired, double guess) {
+		double error = desired - guess;
+
+		for (int i = 0; i < ptrons_num; i++) {
+			for(int j = 0; j <ptrons[i].weights.length; j++)
+			ptrons[i].weights[j] += learningRate * error * ptrons[i].inputs[j];
+		}	/* 잘못된 input이 들어와서 최종 weight를 틀리게 바꿀 수 도 있으므로 해당 input에 완전히 맞도록 weight 바꾸진 않는다
+		 	 * input의 오류를 무시할 수 있도록 weight 변화 횟수를 제한하고 learning rate(c)를 적절한 값으로 지정하는게 point
+		 	 */
+	}
 }
