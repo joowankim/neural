fa2bcbe4b5e0f6ac2fa41176afe6e6a5e8eff8db
kjw217 <32446834+kjw217@users.noreply.github.com>
2017-11-15 01:16:12 +0900

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,10 +1,8 @@
 package neural_network;
 
-import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
 
-public class Trainer extends Frame{
+public class Trainer {
 	double[] inputs;
 	static Painting p;
 
@@ -23,9 +21,6 @@ public class Trainer extends Frame{
 			}
 		}
 		m.forwardInput(inputs);
-		for(i=0; i<m.numOfLayer; i++) {
-			m.layer[i].computeOutput();
-		}
 	}
 	
 	static Trainer[] training = new Trainer[2000];
@@ -41,11 +36,12 @@ public class Trainer extends Frame{
 	
 	public static void main(String[] args) {
 		int layerNum = 1;	// the number of layer
-		int[] eachPtronNum = {1};	// the number of each layer's perceptrons 
+		int[] eachPtronNum = {2};	// the number of each layer's perceptrons
 		double[] example = new double[2];	// input (x, y)
 		int cnt = 0;
 		
 		p = new Painting();
+		
 		mormort = new HiddenLayer(3, eachPtronNum, layerNum);
 		
 		//training
@@ -54,25 +50,27 @@ public class Trainer extends Frame{
 			example[1] = random.nextDouble()*360;
 	
 			double answer = 1;
-			if(example[1]<f(example[0])) answer = -1;
+			if(example[1]<f(example[0])) answer = 0;
 			
 			training[i] = new Trainer(example, mormort);
 			mormort.adjustWeight(answer);
 			
 			p.swing(example, mormort);
-		
+			
 		}
 		
 		
 		// test
 		for(int i=0; i<10; i++) {
-			example[0] = random.nextInt()*320;
-			example[1] = random.nextInt()*180;
+			example[0] = random.nextDouble()*640;
+			example[1] = random.nextDouble()*360;
 			
 			double answer = 1;
-			if(example[1]<f(example[0])) answer = -1;
+			if(example[1]<f(example[0])) answer = 0;
 			
 			testing[i] = new Trainer(example, mormort);
+			
+			//p.swing(example, mormort);
 			
 			System.out.println(i + ". " + "(x,y) : (" + example[0] + ", "+ example[1] + ")");
 			System.out.println("desired : " + answer);
